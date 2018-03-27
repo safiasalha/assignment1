a4411845cff4c3503c5729837bc0c302c559231b
safia <safia@saf.localdomain>
2018-03-28 03:22:51 +0800

    server c

---
+++ cserver1.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<unistd.h>
+
+int main(){
+int clientSocket, newSocket;
+char message[1024];
+struct sockaddr_in serverAddr;
+struct sockaddr_storage serverStorage;
+socklen_t addr_size;
+
+clientSocket = socket(PF_INET,SOCK_STREAM,0);
+
+serverAddr.sin_family=AF_INET;
+
+serverAddr.sin_port=htons(7891);
+serverAddr.sin_addr.s_addr=inet_addr("192.168.171.133");
+
+memset(serverAddr.sin_zero,'\0', sizeof
+serverAddr.sin_zero);
+
+bind(clientSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+if(listen(clientSocket,5)==0)
+{
+printf("Listening...\n");
+}
+else
+printf("Error\n");
+
+addr_size = sizeof serverStorage;
+newSocket = accept(clientSocket, (struct sockaddr *) &serverStorage, 
+&addr_size);
+
+puts("Connection Establish");
+
+recv(newSocket, message, 50, 0);
+char s[] = "assalamualaikum";
+char a[50];
+strlcpy(a,message, 15);
+
+if(strcasecmp(a,s)==0)
+{
+strlcpy(message, "waalaikumussalam", 18);
+}
+else
+
+{strlcpy(message,"Are you a muslim?\n", 15);}
+
+send(newSocket,message, 50,0);
+return 0;
+}
+
+
